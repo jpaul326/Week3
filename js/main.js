a502fdc1c24cc9f9fb5fc02ea25d4151560286c0
Josh Paul <joshpaul@Joshs-MBP-2.fc.ca>
2018-05-30 14:41:46 -0400

    finished js file

--- js/main.js
+++ js/main.js
@@ -14,12 +14,37 @@ comment (when broken and not doing what needed, then needs to be revisited)
 var teams = ["Knights","Caps", "Leafs"]; 
 console.log(teams[0]); //when working with arrays, first position starts at 0 not 1!
 
+	var iconSet = document.querySelectorAll(".icons")
+	console.log(iconSet)
+
 //variables hold things, functions perform actions usually falls under interactivity
 
 function i_am_a_function() {
 	console.log("i_am_a_function");
+	
 }
 
 window.addEventListener("load",i_am_a_function, false);
+
+
+function logLoaded(evt) {
+	//console.log("svg loaded");
+	//console.log(evt);
+	var targetSVG = event.currentTarget.contentDocument;
+	//console.log(targetSVG); 
+	var theIcon = targetSVG.querySelector(".mainIcon");
+	console.log(theIcon);
+	theIcone.addEventListener("click", logClicked, false) 
+}
+
+function logClicked(evt) {
+	console.log(evt.currentTarget.id);
+}
+
+for( var i=0; i<iconSet.length; i++)	{
+		//console.log(i);
+		iconSet[i].addEventListener("load",logLoaded, false);
+}
+
 
 })();
